6f542499ba2b74ba819634626802213ccf7a8f25
huoyuxuan <huoyuxuan@baidu.com>
2023-04-25 19:58:54 +0800

    INF-FE-394 [任务] chat-ai 前端功能
    Change-Id: I53b6e57d490cee7838f1e9c20bcc8433bc561144

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
+    app.enableCors();
+
     await app.listen(parseInt(process.env.PORT ?? '8080', 10) || 8080);
 }
 bootstrap();
